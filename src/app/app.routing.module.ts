af5cf5ce0d719f2fd95f5d51a1a9310aa4cde24d
Kim Kokubun <kim.kokubun@gmail.com>
2022-06-21 20:13:39 -0300

    add not-found page

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,8 +3,10 @@ import {RouterModule, Routes} from "@angular/router";
 
 import {PhotoListComponent} from "./shared/photo/component/photo-list/photo-list.component";
 import {PhotoFormComponent} from "./shared/photo/component/photo-form/photo-form.component";
+import {NotFoundComponent} from "./shared/errors/not-found/not-found.component";
 
 const routes: Routes = [
+  {path: '**', component: NotFoundComponent},
   {path: 'users/flavio', component: PhotoListComponent},
   {path: 'p/add', component: PhotoFormComponent}
 ];

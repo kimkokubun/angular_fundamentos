54b5698cff2a5561c962de4f66ce8b286b449475
Kim Kokubun <kim.kokubun@gmail.com>
2022-06-21 20:37:22 -0300

    add columns rows

--- src/app/shared/photo/component/photo-list/photos/photos.component.ts
+++ src/app/shared/photo/component/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges} from '@angular/core';
 import {Photo} from "../../../photo.interface";
 
 @Component({
@@ -6,12 +6,25 @@ import {Photo} from "../../../photo.interface";
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   @Input() photos: Photo[] = [];
-  constructor() { }
+  rows: any[] = [];
 
-  ngOnInit(): void {
+  constructor() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['photos'])
+      this.rows = this.groupColumns(this.photos);
+  }
+
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3))
+    }
+    return newRows;
   }
 
 }

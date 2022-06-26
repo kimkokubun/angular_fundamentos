9ae919cd861cec5285b421fdc9581545daeb8554
Kim Kokubun <kim.kokubun@gmail.com>
2022-06-26 19:28:06 -0300

    add card shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {PhotosModule} from "./shared/photo/photo.module";
+import {PhotosModule} from "./photo/photo.module";
 import {AppRoutingModule} from "./app.routing.module";
 import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 
-import {PhotoListComponent} from "./shared/photo/component/photo-list/photo-list.component";
-import {PhotoFormComponent} from "./shared/photo/component/photo-form/photo-form.component";
-import {NotFoundComponent} from "./shared/errors/not-found/not-found.component";
-import {PhotoListResolver} from "./shared/photo/component/photo-list/photo-list.resolver";
+import {PhotoListComponent} from "./photo/component/photo-list/photo-list.component";
+import {PhotoFormComponent} from "./photo/component/photo-form/photo-form.component";
+import {NotFoundComponent} from "./errors/not-found/not-found.component";
+import {PhotoListResolver} from "./photo/component/photo-list/photo-list.resolver";
 
 const routes: Routes = [
   {
